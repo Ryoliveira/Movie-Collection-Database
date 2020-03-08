e7055639b7a587abce890f9d246dcaccc9816278
Ryan O <windsofryannn@yahoo.com>
2020-03-07 18:19:40 -0800

    Implemented Lombok to clean up getter/setter, constructor and toString code.

--- src/main/java/com/ombd/OMDBDemo/entity/Movie.java
+++ src/main/java/com/ombd/OMDBDemo/entity/Movie.java
@@ -2,8 +2,17 @@ package com.ombd.OMDBDemo.entity;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Movie {
 
     @Id
@@ -40,136 +49,7 @@ public class Movie {
 
     private boolean isWatchList;
 
-    public Movie() {
-    }
-
-    public Movie(String title, String rated, String year, String released, String runtime, String genre, String plot, String poster, String imdbId) {
-        this.title = title;
-        this.rated = rated;
-        this.year = year;
-        this.released = released;
-        this.runtime = runtime;
-        this.genre = genre;
-        this.plot = plot;
-        this.poster = poster;
-        this.imdbId = imdbId;
-        this.isFavorite = false;
-        this.isWatchList = false;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getRated() {
-        return rated;
-    }
-
-    public void setRated(String rated) {
-        this.rated = rated;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
-
-    public String getReleased() {
-        return released;
-    }
-
-    public void setReleased(String released) {
-        this.released = released;
-    }
-
-    public String getRuntime() {
-        return runtime;
-    }
-
-    public void setRuntime(String runtime) {
-        this.runtime = runtime;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getPlot() {
-        return plot;
-    }
-
-    public void setPlot(String plot) {
-        this.plot = plot;
-    }
-
-    public String getPoster() {
-        return poster;
-    }
-
-    public void setPoster(String poster) {
-        this.poster = poster;
-    }
-
-    public String getImdbId() {
-        return imdbId;
-    }
-
-    public void setImdbId(String imdbId) {
-        this.imdbId = imdbId;
-    }
-
-    public boolean isFavorite() {
-        return isFavorite;
-    }
-
-    public void setFavorite(boolean favorite) {
-        isFavorite = favorite;
-    }
-
-    public boolean isWatchList() {
-        return isWatchList;
-    }
-
-    public void setWatchList(boolean watchList) {
-        isWatchList = watchList;
-    }
-
     public String getImdbLink() {
         return "https://www.imdb.com/title/" + imdbId;
-    }
-
-    @Override
-    public String toString() {
-        return "Movie{" +
-                "id='" + id + '\'' +
-                ", title='" + title + '\'' +
-                ", rated='" + rated + '\'' +
-                ", year='" + year + '\'' +
-                ", released='" + released + '\'' +
-                ", runtime='" + runtime + '\'' +
-                ", genre='" + genre + '\'' +
-                ", plot='" + plot + '\'' +
-                ", poster='" + poster + '\'' +
-                ", imdbId='" + imdbId + '\'' +
-                '}';
     }
 }
--- src/main/java/com/ombd/OMDBDemo/entity/Rating.java
+++ src/main/java/com/ombd/OMDBDemo/entity/Rating.java
@@ -1,7 +1,13 @@
 package com.ombd.OMDBDemo.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Rating {
 
     @JsonProperty("Source")
@@ -10,35 +16,4 @@ public class Rating {
     @JsonProperty("Value")
     private String value;
 
-    public Rating() {
-    }
-
-    public Rating(String source, String value) {
-        this.source = source;
-        this.value = value;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    @Override
-    public String toString() {
-        return "Rating{" +
-                "source='" + source + '\'' +
-                ", value='" + value + '\'' +
-                '}';
-    }
 }
